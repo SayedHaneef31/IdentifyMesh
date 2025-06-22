7763a8941e090485a69e2028d9f10a0c78ed9a63
Sayed Haneef <sayedhaneef31@gmail.com>
2025-06-22 18:44:42 +0530

    Initial setup commit

---
+++ src/main/java/com/moonrider/Smart/Detection/System/SmartDetectionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.moonrider.Smart.Detection.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDetectionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDetectionSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moonrider/Smart/Detection/System/SmartDetectionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moonrider.Smart.Detection.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDetectionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
